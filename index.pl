4fb5412391762cda9c6aebe19afa0af8c6b462fb
zerzal <40403983+zerzal@users.noreply.github.com>
2019-07-11 15:07:10 -0400

    Update index.pl

--- index.pl
+++ index.pl
@@ -25,14 +25,64 @@ print "<html><head><title>AUXCOMM MESSAGING SERVER $ver</title></head>\n";
 print "<body><FONT SIZE = 5><b>AUXCOMM MESSAGING SERVER</b></FONT><FONT SIZE = 2 color = red>\&nbsp\;\&nbsp\;<b>$ver</b><br><br>\n";
 print "* </font><i> = Required fields</i><br><br>\n";
 print "<form method=POST action= $cgiurl>\n";
-print "<FONT SIZE = 2 color = Black> Incident Name (Optional):</font>\&nbsp\;\&nbsp\;\n";
+
+print "<FONT SIZE = 2 color = Black>1. Incident Name (Optional):</font>\&nbsp\;\&nbsp\;\n";
 print "<input id=incident name=incident size=40 type=text><br><br>";
+
+print "<FONT SIZE = 2 color = Black>2. To (Name and Position):</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>";
+
+print "<FONT SIZE = 2 color = Black>3. From (Name and Position):</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>";
+
+print "<FONT SIZE = 2 color = Black>4. Subject:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>";
+
+print "<FONT SIZE = 2 color = Black>5. Date:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>";
+
+print "<FONT SIZE = 2 color = Black>6. Time:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>7. Message:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>8. Approved by: Name:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>Signature:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>Position/Title:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>10. Replied by: Name:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>Position/Title:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>Signature:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
+
+print "<FONT SIZE = 2 color = Black>ICS 213</font>\&nbsp\;\&nbsp\;\n";
+
+print "<FONT SIZE = 2 color = Black>Date/Time:</font>\&nbsp\;\&nbsp\;\n";
+print "<input id=incident name=incident size=40 type=text><br><br>"
 
 
 print "<input type=submit> * <input type=reset><br><br>\n";
 print "</form>";
 
+
+#INCLUDE LINK TO PDF INSTRUCTIONS
+
+
 print "</body></html>\n";
+
+
+
+
 exit;
 
 
